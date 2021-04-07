2a2561f7d7716e817d6113f27c99e8736672d20b
Bhasker Joshi <bhasker.joshi@cubic.com>
2021-04-08 06:35:14 +0800

    updated app with appsync support

--- app/src/main/java/com/prerana/android/swadhishta/MainActivity.java
+++ app/src/main/java/com/prerana/android/swadhishta/MainActivity.java
@@ -25,16 +25,45 @@ package com.prerana.android.swadhishta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.Todo;
 import com.prerana.android.swadhishta.base.BaseActivity;
 import com.prerana.android.swadhishta.codescanner.CodeScannerActivity;
 
 public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+/*
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
+
+            Log.i("Tutorial", "Initialized Amplify");
+        } catch (AmplifyException failure) {
+            Log.e("Tutorial", "Could not initialize Amplify", failure);
+        }
+
+        Amplify.DataStore.observe(Todo.class,
+                started -> Log.i("Tutorial", "Observation began."),
+                change -> Log.i("Tutorial", change.item().toString()),
+                failure -> Log.e("Tutorial", "Observation failed.", failure),
+                () -> Log.i("Tutorial", "Observation complete.")
+        );
+*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.food_product_scanner)
                 .setOnClickListener(v -> startActivity(new Intent(this, CodeScannerActivity.class)));
+
+
     }
 }
